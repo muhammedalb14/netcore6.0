b2b50969e42a3a866aaf5fd05aa568c5d72dc91b
muhammed alb <m003king003@gmail.com>
2024-02-05 13:21:45 +0300

    first netcore 6.0

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace GB2.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult About(int x,int y)
+        {
+            ViewBag.sum = x + y;
+            return View();
+        }
+    }
+}
